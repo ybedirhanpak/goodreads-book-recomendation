c35796179c062fdbc0f931828f485530441ae163
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2021-01-19 00:25:01 +0300

    utils.py: Add normalize_doc and normalize_token

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,10 @@
 import os
 import pickle
+import re
+
+ALPHABET = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+BOOKS_BASE_URL = "https://www.goodreads.com/book/show/"
 
 
 def create_dir(dir_name: str):
@@ -22,6 +27,7 @@ def run_threads_and_wait(threads: list):
     for thread in threads:
         thread.join()
 
+
 def pickle_object(object, location):
     with open(get_file_path(location), 'wb') as file:
         pickle.dump(object, file, protocol=pickle.HIGHEST_PROTOCOL)
@@ -31,3 +37,17 @@ def unpickle_object(location):
     with open(get_file_path(location), 'rb') as file:
         obj = pickle.load(file)
     return obj
+
+
+def normalize_doc(doc: str):
+    '''
+        Replaces non-alphabet characters with space character
+    '''
+    return re.sub(f'[^{ALPHABET}]', ' ', doc.lower()).strip()
+
+
+def normalize_token(token: str):
+    '''
+        Normalizes given token by removing trailing spaces 
+    '''
+    return token.strip()
