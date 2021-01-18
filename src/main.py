4247d475e7b0e0e43fadc3b850f4c6a37addfeee
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2021-01-18 18:25:38 +0300

    main.py: Give books.txt and books.pickle as param

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 from download import BookDownloader
 
+BOOKS_FILE = "data/books.txt"
+BOOKS_PICKLE = "out/books.pickle"
+
 # Download books
-book_downloader = BookDownloader()
-book_downloader.download_books()
+book_downloader = BookDownloader(books_pickle=BOOKS_PICKLE)
+book_downloader.download_books(books_file=BOOKS_FILE)
