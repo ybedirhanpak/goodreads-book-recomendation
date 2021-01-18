541d637a7bcc9061b39b71e5c964406d1cddcfc8
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2021-01-19 00:37:11 +0300

    main.py: Add preprocessing

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from download import BookDownloader
+from preprocess import BookPreprocessor
 
 BOOKS_FILE = "data/books.txt"
 BOOKS_PICKLE = "out/books.pickle"
@@ -6,3 +7,7 @@ BOOKS_PICKLE = "out/books.pickle"
 # Download books
 book_downloader = BookDownloader(books_pickle=BOOKS_PICKLE)
 book_downloader.download_books(books_file=BOOKS_FILE)
+
+# Preprocess books
+book_preprocessor = BookPreprocessor(books_pickle=BOOKS_PICKLE)
+book_preprocessor.preprocess_books()
