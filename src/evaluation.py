2c2377c14854cca731cb78a1673199ed9d2d81fc
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2021-01-20 16:37:45 +0300

    evaluation.py: Update calculation of precision

--- src/evaluation.py
+++ src/evaluation.py
@@ -2,12 +2,9 @@ from typing import List
 
 
 def evaluate_precision(original_list: List, calculated_list: List):
-    precision = len(
-        [book_url for book_url in calculated_list if book_url in original_list]) / len(original_list)
-
     average_precision = 0
     relevant_count = 0
-    precision_list = []
+    precision = 0
     precision_sum = 0
     for index, book_url in enumerate(calculated_list):
         if book_url in original_list:
@@ -15,7 +12,6 @@ def evaluate_precision(original_list: List, calculated_list: List):
         precision = relevant_count / (index + 1)
         if book_url in original_list:
             precision_sum += precision
-        precision_list.append(precision)
 
     if relevant_count > 0:
         average_precision = precision_sum / relevant_count
